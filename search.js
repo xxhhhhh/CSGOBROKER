015da3ccb5449c70b77eb48e07f6e92d457f0e56
xxhhhhh <webmoneySifo@yandex.ru>
2023-05-20 03:28:34 +0300

    add filter-searcher

--- search.js
+++ search.js
@@ -1,37 +1,176 @@
-document.getElementById('search-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // Предотвращаем отправку формы
+var siteList = document.getElementById('site-list');
+var searchInput = document.getElementById('search-input'); // Переместите это объявление вверх
+var isRussianPage = window.location.pathname.includes('/ru'); // Проверяем, содержится ли в пути "/ru/"
+var sites = [
+'<li><a href="https://csgobroker.cc/reviews/idle-empire">Idle-empire</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/insanegg">Insanegg</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/key-drop">Key-drop</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/knifex">Knifex</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/lis-skins">Lis-skins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/lootbear">Lootbear</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/lootfarm">Lootfarm</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/primedice">Primedice</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rollbit">Rollbit</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/roobet">Roobet</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustbet">Rustbet</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustcases">Rustcases</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustchance">Rustchance</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustclash">Rustclash</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustix">Rustix</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustmoment">Rustmoment</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/ruststake">Ruststake</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustyloot">Rustyloot</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/rustypot">Rustypot</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/salad">Salad</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/shadowpay">Shadowpay</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/skinbaron">Skinbaron</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/skinbet">Skinbet</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/skincashier">Skincashier</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/skinscash">Skinscash</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/skinswap">Skinswap</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/steamgifts">Steamgifts</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/steamlvlup">Steamlvlup</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/swapgg">Swapgg</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/tradeit">Tradeit</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/vvvgamers">Vvvgamers</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/wtfskins">Wtfskins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/xplay">Xplay</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/avanmarket">Avanmarket</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/banditcamp">Banditcamp</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/bcgame">Bcgame</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/bets4pro">Bets4pro</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/bitskins">Bitskins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/bitskins-p2p">Bitskins p2p</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/clashgg">Clashgg</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/crashgg">Crashgg</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csdeals">CsDeals</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgo500">CSGO500</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgobig">CSGOBig</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgoempire">CSGOEmpire</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgofast">CSGOFast</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgolive">CSGOLive</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgoluck">CSGOLuck</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgo-market">CSGO-Market</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgopolygon">CSGOPolygon</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgopositive">CSGOPositive</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgoroll">CSGORoll</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgoselly">CSGOSelly</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/csgo-skins">CSGO-Skins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/cybershoke">Cybershoke</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/daddyskins">Daddyskins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/datdrop">Datdrop</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/dmarket">Dmarket</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/duelbits">Duelbits</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/earnweb">Earnweb</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/farmskins">Farmskins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/flamecases">Flamecases</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/freecash">Freecash</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/freeward">Freeward</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/gamdom">Gamdom</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/gamehag">Gamehag</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/gamerpay">Gamerpay</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/gametame">Gametame</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/gcskins">Gcskins</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/grindbux">Grindbux</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/hellcase">Hellcase</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/hellstore">Hellstore</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/howlgg">Howlgg</a></li>',
+'<li><a href="https://csgobroker.cc/reviews/hypeup">Hypeup</a></li>'
+];
 
-  var searchTerm = document.getElementById('search-input').value;
-  searchPages(searchTerm);
-});
+function compareSites(a, b) {
+  var siteNameA = a.match(/<a href=".*?">(.*?)<\/a>/)[1].toLowerCase();
+  var siteNameB = b.match(/<a href=".*?">(.*?)<\/a>/)[1].toLowerCase();
+  var searchTerm = searchInput.value.toLowerCase();
 
-function searchPages(searchTerm) {
-  var pages = document.getElementsByTagName('a');
-  var resultsContainer = document.getElementById('search-results');
-  var results = [];
-
-  for (var i = 0; i < pages.length; i++) {
-    var page = pages[i];
-    if (page.textContent.toLowerCase().includes(searchTerm.toLowerCase())) {
-      results.push(page);
-    }
-  }
-
-  // Очищаем контейнер с результатами
-  while (resultsContainer.firstChild) {
-    resultsContainer.removeChild(resultsContainer.firstChild);
-  }
-
-  // Отображаем результаты поиска
-  if (results.length > 0) {
-    for (var j = 0; j < results.length; j++) {
-      var result = document.createElement('p');
-      result.textContent = results[j].textContent;
-      resultsContainer.appendChild(result);
-    }
+  if (siteNameA.charAt(0) === searchTerm.charAt(0) && siteNameB.charAt(0) !== searchTerm.charAt(0)) {
+      return -1;
+  } else if (siteNameA.charAt(0) !== searchTerm.charAt(0) && siteNameB.charAt(0) === searchTerm.charAt(0)) {
+      return 1;
   } else {
-    var noResults = document.createElement('p');
-    noResults.textContent = 'Ничего не найдено.';
-    resultsContainer.appendChild(noResults);
+      return siteNameA.localeCompare(siteNameB);
   }
 }
+
+function updateSiteList() {
+  siteList.innerHTML = '';
+  sites.sort(compareSites);
+
+  sites.forEach(function(site) {
+    var li = document.createElement('li');
+    li.className = 'site-item';
+    li.style.display = 'none';
+    li.innerHTML = site;
+    
+    var link = li.querySelector('a');
+    
+    // Добавляем "/ru/" после "csgobroker.cc/" к ссылке, если находимся на русской странице
+    if (isRussianPage) {
+      var href = link.getAttribute('href');
+      var newHref = href.replace('csgobroker.cc/', 'csgobroker.cc/ru/');
+      link.setAttribute('href', newHref);
+    }
+    
+    li.innerHTML = '';
+    li.appendChild(link);
+    
+    siteList.appendChild(li);
+  });
+}
+
+function hideAllSites(siteItems) {
+  for (var i = 0; i < siteItems.length; i++) {
+      var siteItem = siteItems[i];
+      hideSite(siteItem);
+  }
+}
+
+function hideSite(siteItem) {
+  siteItem.style.display = 'none';
+}
+
+function showSite(siteItem) {
+  siteItem.style.display = 'flex';
+}
+
+function handleSearchInput() {
+  var searchTerm = searchInput.value.toLowerCase();
+  var siteItems = siteList.getElementsByClassName('site-item');
+
+  if (searchTerm === '') {
+      hideAllSites(siteItems);
+      siteList.style.display = 'none';
+      return;
+  }
+
+  for (var i = 0; i < siteItems.length; i++) {
+      var siteItem = siteItems[i];
+      var siteName = siteItem.textContent.toLowerCase();
+
+      if (siteName.startsWith(searchTerm)) {
+          showSite(siteItem);
+      } else {
+          hideSite(siteItem);
+      }
+  }
+
+  siteList.style.display = 'block';
+}
+
+searchInput.addEventListener('input', handleSearchInput);
+
+searchInput.addEventListener('focus', function() {
+  if (searchInput.value === '') {
+      siteList.style.display = 'none';
+  } else {
+      siteList.style.display = 'block';
+  }
+});
+
+searchInput.addEventListener('blur', function() {
+  setTimeout(function() {
+      siteList.style.display = 'none';
+  }, 150);
+});
+
+updateSiteList();
