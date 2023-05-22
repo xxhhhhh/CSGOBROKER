4f6d2d0fbe31edfb399993efcf36c410aad28e67
xxhhhhh <webmoneySifo@yandex.ru>
2023-05-23 01:38:18 +0300

    fasttest

---
+++ lang2.js
@@ -0,0 +1,66 @@
+// Проверяем, был ли ранее сделан выбор пользователем
+var userChoice = getCookie('languageChoice');
+
+if (!userChoice && window.location.hostname !== 'localhost') {
+  // Получаем языковую настройку браузера пользователя
+  var userLang = navigator.language || navigator.userLanguage;
+
+  // Проверяем значение языковой настройки и перенаправляем на соответствующую страницу
+  if ((userLang === 'ru' || userLang === 'ru-RU') && !window.location.href.includes('/ru/')) {
+    // Получаем текущий URL-адрес
+    var currentUrl = window.location.href;
+
+    if (!currentUrl.includes('/ru/')) {
+      currentUrl = currentUrl.replace(/\/ru/g, '');
+
+      // Формируем новый URL-адрес с добавленным '/ru/'
+      var newUrl = 'https://csgobroker.cc/ru/' + currentUrl.substr('https://csgobroker.cc/'.length);
+
+      // Перенаправляем на новый URL-адрес
+      window.location.href = newUrl;
+    }
+  }
+}
+
+// Обработчик события клика на элементах меню выбора языка
+document.addEventListener('click', function(event) {
+  // Проверяем, был ли клик на элементе с классом "lang-switch"
+  if (event.target.classList.contains('lang-switch')) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+
+    var selectedLang = event.target.dataset.lang; // Получаем выбранный язык из атрибута "data-lang"
+
+    // Устанавливаем куку "languageChoice" с выбранным языком на 365 дней
+    setCookie('languageChoice', selectedLang, 365);
+
+    // Перезагружаем страницу для применения изменений языка
+    location.reload();
+  }
+});
+
+// Функция для установки куки
+function setCookie(name, value, days) {
+  var expires = '';
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
+}
+
+// Функция для получения значения куки
+function getCookie(name) {
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nameEQ) === 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
+}
