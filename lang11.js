53ac1086251f55d619457713701a010a3aca9003
xxhhhhh <webmoneySifo@yandex.ru>
2023-05-23 02:13:18 +0300

    hf2

--- lang11.js
+++ lang11.js
@@ -1,39 +1,30 @@
 function handleLanguageRedirect() {
-  // Проверяем, был ли ранее сделан выбор пользователем
   var userChoice = getCookie('languageChoice');
 
   if (!userChoice && window.location.hostname !== 'localhost') {
-    // Получаем языковую настройку браузера пользователя
     var userLang = navigator.language || navigator.userLanguage;
 
-    // Проверяем значение языковой настройки и перенаправляем на соответствующую страницу
     if ((userLang === 'ru' || userLang === 'ru-RU') && !window.location.pathname.includes('/ru/')) {
-      // Получаем текущий URL-адрес
       var currentUrl = window.location.href;
 
-      // Проверяем, является ли текущая страница https://csgobroker.cc/
-      if (currentUrl === 'https://csgobroker.cc/') {
-        // Формируем новый URL-адрес с добавленным '/ru'
-        var newUrl = currentUrl + 'ru';
+      if (currentUrl === 'https://csgobroker.cc/' || currentUrl === 'https://csgobroker.cc/ru/') {
+        var url = new URL(window.location.href);
+        url.pathname = '/ru';
+        var newUrl = url.href;
 
-        // Проверяем, является ли новый URL-адрес отличным от текущего
         if (newUrl !== currentUrl) {
-          // Перенаправляем на новый URL-адрес
           window.location.href = newUrl;
-          return false; // Завершаем выполнение кода, чтобы предотвратить выполнение остальных действий
+          return false;
         }
       } else {
-        // Удаляем первый слэш из пути, если есть
         var newPath = window.location.pathname.replace(/^\//, '');
 
-        // Проверяем, отсутствует ли уже '/ru/' в текущем пути
         if (!newPath.startsWith('ru/')) {
-          // Формируем новый URL-адрес с добавленным '/ru/'
-          var newUrl = window.location.origin + '/ru/' + newPath;
+          var url = new URL(window.location.href);
+          url.pathname = '/ru/' + newPath;
+          var newUrl = url.href;
 
-          // Проверяем, является ли новый URL-адрес отличным от текущего
           if (newUrl !== window.location.href) {
-            // Перенаправляем на новый URL-адрес
             window.location.href = newUrl;
           }
         }
