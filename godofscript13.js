1eb118e75582f2c102d260c98cbab686d1b8418a
xxhhhhh <webmoneySifo@yandex.ru>
2024-02-20 00:15:14 +0300

    minor fixes

--- godofscript13.js
+++ godofscript13.js
@@ -2623,37 +2623,40 @@ window.addEventListener('resize', updateCategoryBoxHrefs);
         })();
     
         function insertRandomAdsBox() {
-            var currentPath = window.location.pathname;
-    
-            if (currentPath.includes('/topic/skins/') && currentPath.includes('/ru/')) {
-                var adsFilePath = '/code-parts/topic-ads-ru.html';
-            } else if (currentPath.includes('/topic/skins/')) {
-                var adsFilePath = '/code-parts/topic-ads.html';
-            } else {
-                return;
-            }
-    
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', adsFilePath, true);
-    
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    var adsBoxesHtml = xhr.responseText;
-                    var adsBoxes = document.createElement('div');
-                    adsBoxes.innerHTML = adsBoxesHtml;
-    
-                    var insertAfterElement = document.querySelector('.box-topic');
-    
-                    var randomAdsBox = adsBoxes.children[Math.floor(Math.random() * adsBoxes.children.length)];
-    
-                    insertAfterElement.parentNode.insertBefore(randomAdsBox, insertAfterElement.nextSibling);
-                }
-            };
-    
-            xhr.send();
-        }
-    
-        insertRandomAdsBox();
+          var currentPath = window.location.pathname;
+      
+          if (currentPath.includes('/topic/skins/') && currentPath.includes('/ru/')) {
+              var adsFilePath = '/code-parts/topic-ads-ru.html';
+          } else if (currentPath.includes('/topic/skins/')) {
+              var adsFilePath = '/code-parts/topic-ads.html';
+          } else {
+              return;
+          }
+      
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', adsFilePath, true);
+      
+          xhr.onreadystatechange = function () {
+              if (xhr.readyState == 4 && xhr.status == 200) {
+                  var adsBoxesHtml = xhr.responseText;
+                  var adsBoxes = document.createElement('div');
+                  adsBoxes.innerHTML = adsBoxesHtml;
+      
+                  var insertAfterElement = document.querySelector('.box-topic');
+      
+                  var randomAdsBox = adsBoxes.children[Math.floor(Math.random() * adsBoxes.children.length)];
+      
+                  randomAdsBox.classList.add('fade-in');
+      
+                  insertAfterElement.parentNode.insertBefore(randomAdsBox, insertAfterElement.nextSibling);
+              }
+          };
+      
+          xhr.send();
+      }
+      
+      insertRandomAdsBox();
+      
     };
     
     document.addEventListener('DOMContentLoaded', function () {
