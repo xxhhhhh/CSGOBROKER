984ff231f48fe2c580cfe033dc0dbc2f9575d1c5
xxhhhhh <webmoneySifo@yandex.ru>
2024-04-15 09:43:33 +0300

    test script opt

--- godofscript34.js
+++ godofscript34.js
@@ -2234,90 +2234,37 @@ if (window.location.href.indexOf('/reviews/') === -1 && window.location.href.ind
 
 const cachedContent = {};
 
-if (
-  window.location.href.endsWith("/csgo/buy-skins") ||
-  window.location.href.endsWith("/csgo/sell-skins") ||
-  window.location.href.endsWith("/csgo/trade-skins") ||
-  window.location.href.endsWith("/csgo/instant-sell") ||
-  window.location.href.endsWith("/csgo/marketplaces") ||
-  window.location.href.endsWith("/csgo/buy-skins.html") ||
-  window.location.href.endsWith("/csgo/sell-skins.html") ||
-  window.location.href.endsWith("/csgo/trade-skins.html") ||
-  window.location.href.endsWith("/csgo/marketplaces.html") ||
-  window.location.href.endsWith("/csgo/instant-sell.html")
-) {
-  importModsBox("csgo-skins") + importModsBox("csgo");
+function getCategoryFromUrl(url) {
+  const match = url.match(/\/(csgo|rust|dota)\/(buy|sell|trade|instant-sell|marketplaces)/);
+  if (match) {
+    return { game: match[1], action: match[2] };
+  } else if (url.includes("/freebies/")) {
+    return { game: "freebies" };
+  } else if (url.includes("/crypto/")) {
+    return { game: "crypto" };
+  } else if (url.includes("/csgo/") || url.includes("/rust/") || url.includes("/dota/")) {
+    return { game: url.split("/")[3] };
+  }
+  return null;
 }
-else if (
-  window.location.href.endsWith("/rust/buy-skins") ||
-  window.location.href.endsWith("/rust/sell-skins") ||
-  window.location.href.endsWith("/rust/trade-skins") ||
-  window.location.href.endsWith("/rust/instant-sell") ||
-  window.location.href.endsWith("/rust/marketplaces") ||
-  window.location.href.endsWith("/rust/buy-skins.html") ||
-  window.location.href.endsWith("/rust/sell-skins.html") ||
-  window.location.href.endsWith("/rust/trade-skins.html") ||
-  window.location.href.endsWith("/rust/marketplaces.html") ||
-  window.location.href.endsWith("/rust/instant-sell.html")
-) {
-  importModsBox("rust-skins") + importModsBox("rust");
-}
-else if (
-  window.location.href.endsWith("/dota/buy-items") ||
-  window.location.href.endsWith("/dota/sell-items") ||
-  window.location.href.endsWith("/dota/trade-items") ||
-  window.location.href.endsWith("/dota/instant-sell") ||
-  window.location.href.endsWith("/dota/marketplaces") ||
-  window.location.href.endsWith("/dota/buy-items.html") ||
-  window.location.href.endsWith("/dota/sell-items.html") ||
-  window.location.href.endsWith("/dota/trade-items.html") ||
-  window.location.href.endsWith("/dota/marketplaces.html") ||
-  window.location.href.endsWith("/dota/instant-sell.html")
-) {
-  importModsBox("dota-items") + importModsBox("dota");
-}
-else if (
-  window.location.href.includes("/freebies/") ||
-  window.location.href.endsWith("/freebies") ||
-  window.location.href.endsWith("/freebies.html")
-) {
-  importModsBox("freebies");
-}
-else if (
-  window.location.href.includes("/crypto/") ||
-  window.location.href.endsWith("/crypto") ||
-  window.location.href.endsWith("/crypto.html")
-) {
-  importModsBox("crypto");
-}
-else if (
-  window.location.href.includes("/csgo/") ||
-  window.location.href.endsWith("/ru") ||
-  window.location.href.endsWith("/es") ||
-  window.location.href.endsWith("/tr") ||
-  window.location.href.endsWith("/pt") ||
-  window.location.href.endsWith("/hi") ||
-  window.location.href.endsWith("/")   ||
-  window.location.href.endsWith("index.html")   ||
-  window.location.href.endsWith("/ru.html") ||
-  window.location.href.endsWith("/es.html") ||
-  window.location.href.endsWith("/tr.html") ||
-  window.location.href.endsWith("/pt.html") ||
-  window.location.href.endsWith("/hi.html")
-) {
-  importModsBox("csgo");
-} else if (
-  window.location.href.includes("/rust/") ||
-  window.location.href.endsWith("/rust") ||
-  window.location.href.endsWith("/rust.html")
-) {
-  importModsBox("rust");
-} else if (
-  window.location.href.includes("/dota/") ||
-  window.location.href.endsWith("/dota") ||
-  window.location.href.endsWith("/dota.html")
-) {
-  importModsBox("dota");
+
+const category = getCategoryFromUrl(window.location.href);
+if (document.querySelector('.mods-box')) {
+  if (category) {
+    if (category.game === "csgo") {
+      importModsBox("csgo-skins") + importModsBox("csgo");
+    } else if (category.game === "rust") {
+      importModsBox("rust-skins") + importModsBox("rust");
+    } else if (category.game === "dota") {
+      importModsBox("dota-items") + importModsBox("dota");
+    } else if (category.game === "freebies") {
+      importModsBox("freebies");
+    } else if (category.game === "crypto") {
+      importModsBox("crypto");
+    }
+  } else {
+    importModsBox("csgo");
+  }
 }
 
 function importModsBox(boxId) {
