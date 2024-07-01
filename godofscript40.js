c45466bad413e50b445a464490cb55cffc5d3e18
xxhhhhh <webmoneySifo@yandex.ru>
2024-07-01 20:48:34 +0300

    Revert "remove canonical +worker"
    This reverts commit 17a7f4a25440949926e832941da205c91bb72509.

--- godofscript40.js
+++ godofscript40.js
@@ -337,8 +337,7 @@ document.addEventListener('DOMContentLoaded', function() {
     !window.location.pathname.endsWith("/tr.html") &&
     !window.location.pathname.endsWith("/hi.html") &&
     !window.location.pathname.endsWith("/404.html") &&
-    !window.location.pathname.endsWith("/index.html") &&
-    !window.location.pathname.endsWith("/")
+    !window.location.pathname.endsWith("/index.html")
   ) {
     var currentLanguage = languageTag;
     var supportedLanguages = ["en", "ru", "pt", "es", "tr", "hi"];
