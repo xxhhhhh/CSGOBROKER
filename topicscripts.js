5157ecce3b545b0145cf9ac28253f17635158dcd
xxhhhhh <webmoneySifo@yandex.ru>
2025-02-28 10:09:24 +0300

    upd cache + upd avanmarket logo

--- topicscripts.js
+++ topicscripts.js
@@ -49,7 +49,7 @@ $(document).ready(function () {
                       .map(
                         (site) => `
                     <div class="site-searcher-box" id="${site}" data-title="${
-                          languageTag === "ru" ? "Искать в" : "Search on"
+                          languageTag === "ru" ? "Искать на" : "Search on"
                         } ${site}">
                         <div class="site-searcher-logo">
                             <img src="/img/${site
